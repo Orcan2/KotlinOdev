d11d9d8d24cf63c46f98e6ed6eeaa7b44a33519c
ORCAN <hasanorcang@gmail.com>
2023-07-26 20:48:15 +0300

    Update Main.kt

--- 26temmuz/Main.kt
+++ 26temmuz/Main.kt
@@ -5,40 +5,20 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
-open class GeometrikSekil {
+
+open class Arac(){
+   open val Arac_hizi:Int=0
+   open var Yakit="yakit"
 
 
-
-   open fun Geometrik_seklin_ozelligi(){
-
-    }
-}
-class Dikdortgen:GeometrikSekil(){
-   override fun Geometrik_seklin_ozelligi(){
-        var boy:Float
-        var en:Float
-        var alan:Float
-        var cevre:Float
-        println("En ve boy giriniz")
-        boy = readln().toFloat()
-        en= readln().toFloat()
-        cevre=2*(boy+en)
-        alan=boy*en
-        println("Cevre $cevre , alan $alan")
-    }
 
 }
-class Daire:GeometrikSekil(){
-   override fun Geometrik_seklin_ozelligi(){
-        var yaricap:Float
-        var pi:Float=3.14f
-        var alan:Float
-        var cevre:Float
-        println("yaricap giriniz")
-        yaricap = readln().toFloat()
+class Otomobil:Arac(){
+    override val Arac_hizi: Int=150
 
-        cevre=2*pi*yaricap
-        alan=pi*yaricap*yaricap
-        println("Cevre $cevre , alan $alan")
+    override var Yakit: String="Dizel"
+
+    fun bagaj(){
+        println("Bagaj Açıldı")
     }
 }
